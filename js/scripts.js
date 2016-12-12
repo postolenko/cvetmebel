d69db6e9fcaf69baac070ef2f058ebfbb73fd105
postolenko <denis.postolenko@yandex.ua>
2016-12-12 05:32:48 +0200

    Cvetmebel

--- js/scripts.js
+++ js/scripts.js
@@ -45,6 +45,142 @@ $(document).ready(function() {
     });
 
 
+    // ----------------------------------------------------------------
+
+    $(function() {
+
+        $(".inner-nav-wrapp").addClass("js");
+
+        var indexNavItem;
+
+        var innerNavHeight;
+
+        // $(".nav-item").bind( "mouseenter mouseleave", function() {
+
+        //     indexNavItem = $(".nav-item").index(this);
+
+        //     innerNavHeight = $(".nav-item:eq("+ indexNavItem +") > .inner-nav-wrapp > .inner-nav").height();
+
+        //     if( $(".nav-item:eq("+ indexNavItem +") > .inner-nav-wrapp").height() <= 0 ) {
+
+        //         $(".nav-item:eq("+ indexNavItem +") > .inner-nav-wrapp").animate({"height" : innerNavHeight + "px"}, 200);
+
+        //     } else {
+
+        //         $(".nav-item:eq("+ indexNavItem +") > .inner-nav-wrapp").animate({"height" : 0 + "px"}, 200);
+
+        //     }
+
+        // });
+
+        $(".nav-item").bind({
+
+              mouseenter: function() {
+
+                indexNavItem = $(".nav-item").index(this);
+
+                innerNavHeight = $(".nav-item:eq("+ indexNavItem +") > .inner-nav-wrapp > .inner-nav").height();
+                
+                if( $(".nav-item:eq("+ indexNavItem +") > .inner-nav-wrapp").height() <= 0 ) {
+
+                    $(".nav-item:eq("+ indexNavItem +") > .inner-nav-wrapp").animate({"height" : innerNavHeight + "px"}, 300);
+
+                }
+
+              },
+              mouseleave: function() {
+
+                indexNavItem = $(".nav-item").index(this);
+
+                innerNavHeight = $(".nav-item:eq("+ indexNavItem +") > .inner-nav-wrapp > .inner-nav").height();
+                
+                if( $(".nav-item:eq("+ indexNavItem +") > .inner-nav-wrapp").height() > 0 ) {
+
+                    $(".nav-item:eq("+ indexNavItem +") > .inner-nav-wrapp").animate({"height" : 0 + "px"}, 300);
+
+                }
+
+              }
+        });
+
+    });
+
+    // ----------------------------------------------------------------
+
+
+    $(function() {
+
+
+        $(".respmenubtn").click(function() {
+
+            $(".resp-main-navigation").fadeIn(300);
+
+        });
+
+
+        $(".close-resp-nav").click(function() {
+
+            $(".resp-main-navigation").fadeOut(300);
+
+        });
+
+    });
+
+
+    $(function() {
+
+        var respLinkIndex;
+
+        var respInnerNavHeight;
+
+        $(".resp-nav-btn").click(function() {
+
+            $(this).toggleClass("active");
+
+            respLinkIndex = $(".resp-nav-btn").index(this);
+
+            respInnerNavHeight = $(".resp-inner-nav:eq("+ respLinkIndex +")").outerHeight(true);
+
+            if( $(".resp-inner-nav-wrapp:eq("+ respLinkIndex +")").height() <= 0 ) {
+
+                $(".resp-inner-nav-wrapp:eq("+ respLinkIndex +")").animate({"height" : respInnerNavHeight + "px"}, 200);
+
+                setTimeout(function() {
+
+                    $(".resp-inner-nav-wrapp:eq("+ respLinkIndex +")").css({"height" : "auto"});
+
+                }, 300);
+
+            } else {
+
+                $(".resp-inner-nav-wrapp:eq("+ respLinkIndex +")").animate({"height" : 0 + "px"}, 200);
+
+            }
+
+
+        });
+
+    });
+
+    // ----------------------------------------------------------------
+
+    $(function() {
+
+        $(".sroll-down").click(function () {
+
+            $("body,html").animate({
+
+                scrollTop: $("#scroll_down_anchor").offset().top
+
+            }, 1000);
+
+            return false;
+
+        });
+
+    });
+
+    // ----------------------------------------------------------------
 
     // function getFooterPosition() {
 
